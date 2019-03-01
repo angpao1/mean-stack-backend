54c673750304806da4d2c45e734fd8ad68fccc9c
angpao <angpao.kittimasak@gmail.com>
2019-03-01 13:32:54 +0700

    Function Update Product Done

--- routes/product.route.js
+++ routes/product.route.js
@@ -30,4 +30,26 @@ productRoutes.route('/add').post((req, res) => {
     })
 })
 
+productRoutes.route('/update/:id').post((req, res) => {
+    mongoose.connect(config.DB)
+    Product.findById(req.params.id, (err, product) => {
+        if (!product) {
+            mongoose.connection.close()
+            return next(new Error('Could not load Document'))
+        } else {
+            product.productName = req.body.productName
+            product.category = req.body.category
+            product.price = req.body.price
+
+            product.save().then(data => {
+                res.status(200).json('Update Product Success')
+                mongoose.connection.close()
+            }).catch(err => {
+                res.status(400).json('cannot to update the database')
+                mongoose.connection.close()
+            })
+        }
+    })
+})
+
 module.exports = productRoutes
